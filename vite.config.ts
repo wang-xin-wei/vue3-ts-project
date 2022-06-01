d19abc48adb7c04c15a17b618a004f4c172d5a85
wang-xin-wei <327429246@qq.com>
2022-06-01 18:22:42 +0800

    add jsx/tsx support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    eslintPlugin({
+      // 配置选项
+      cache: false // 禁用eslint缓存
+    }),
+    vueJsx({
+      // 配置选项
+    })
+  ]
 })
